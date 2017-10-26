8195192eef01f907888dd9fbb402f8bcfcf52db8
ZhangZiyun <SA515035@mail.ustc.edu.cn>
2017-10-26 12:55:53 -0400

    Initial Commit

---
+++ 6050_assn2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  6050_assn2
+//
+//  Created by 张子筠 on 10/26/17.
+//  Copyright © 2017 ziyunz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
